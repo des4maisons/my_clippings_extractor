cf3ab71a54c3c3b228ec83da1529517333f0f181
Marguerite des Trois Maisons <des4maisons+github@gmail.com>
2016-05-28 13:11:02 -0400

    Add code to dump extracted data into sqlite db

--- extract-to-sqlite.py
+++ extract-to-sqlite.py
@@ -2,7 +2,7 @@
 import sys
 import kindle_clippings_lexer
 from kindle_clippings_parser import yacc
-from pprint import PrettyPrinter
+from sqlite_dumper import SqliteDumper
 
 def get_content(fname):
     with open(fname) as f:
@@ -13,5 +13,6 @@ if __name__ == '__main__':
     if len(sys.argv) != 2 or sys.argv[1] == '-h' or sys.argv[1] == '--help':
         print('Usage: ' + sys.argv[0] + ' /path/to/my_clippings.txt', file=sys.stderr)
         sys.exit(1)
-
-    PrettyPrinter(indent=4).pprint(yacc.parse(get_content(sys.argv[1])))
+    data = yacc.parse(get_content(sys.argv[1]))
+    d = SqliteDumper(file='foo.db')
+    d.store(data)
